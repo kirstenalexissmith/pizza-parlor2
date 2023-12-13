a83aaab01b658ca7f164f579f3aa23b65c567906
Kirsten Smith <kirstenalexis7@gmail.com>
2023-12-13 10:24:24 -0800

    init repo

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function pizzaFormHandler(e) {
+  e.preventDefault();
+  const selectedToppings = Array.from(document.querySelectorAll('input[name="topping"]:checked')).map(checkbox => checkbox.value);
+  const selectedSize = document.querySelector('input[name="size"]:checked');
+  const pizzaOrder = new Pizza(selectedSize, selectedToppings)
+  const cost = pizzaOrder.getPizzaCost();
+  if (selectedSize === null) {
+    document.getElementById('error-message').innerText = 'Please select a size option!';
+  } else {
+    document.getElementById("pizza-cost").textContent = `$${cost}`
+  }
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("#form").addEventListener("submit", pizzaFormHandler)
+
+  const refresh = document.getElementById("refresh");
+  refresh.addEventListener("click", function () {
+    location.reload();
+  })
+})
